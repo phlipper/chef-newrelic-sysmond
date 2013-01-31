de224159f63664f651a485506e4051ac8eae1625
Phil Cohen <github@phlippers.net>
2013-01-31 12:51:36 -0800

    ensure a license key exists before installing

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,25 +5,29 @@
 # Copyright 2011-2012, Phil Cohen
 #
 
-apt_repository "newrelic" do
-  uri "http://apt.newrelic.com/debian/"
-  components ["newrelic", "non-free"]
-  key "548C16BF"
-  keyserver node["new_relic"]["keyserver"]
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
-end
+unless node["new_relic"]["license_key"].empty?
 
-package "newrelic-sysmond"
+  apt_repository "newrelic" do
+    uri "http://apt.newrelic.com/debian/"
+    components ["newrelic", "non-free"]
+    key "548C16BF"
+    keyserver node["new_relic"]["keyserver"]
+    action :add
+    notifies :run, "execute[apt-get update]", :immediately
+  end
 
-template "/etc/newrelic/nrsysmond.cfg" do
-  source "nrsysmond.cfg.erb"
-  owner "root"
-  group "newrelic"
-  mode 0640
-  notifies :restart, "service[newrelic-sysmond]"
-end
+  package "newrelic-sysmond"
 
-service "newrelic-sysmond" do
-  action [:enable, :start]
+  template "/etc/newrelic/nrsysmond.cfg" do
+    source "nrsysmond.cfg.erb"
+    owner "root"
+    group "newrelic"
+    mode 0640
+    notifies :restart, "service[newrelic-sysmond]"
+  end
+
+  service "newrelic-sysmond" do
+    action [:enable, :start]
+  end
+
 end
