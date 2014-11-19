eb64179466e4172922120ee162eb46d2496b84d0
Jose Olcese <jolcese@gmail.com>
2014-11-19 12:05:37 -0800

    Update default.rb
    Adding labels

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,3 +22,4 @@ default["new_relic"]["pidfile"]        = ""
 default["new_relic"]["collector_host"] = "collector.newrelic.com"
 default["new_relic"]["timeout"]        = 30
 default["new_relic"]["hostname"]       = ""
+default["new_relic"]["labels"]         = ""
