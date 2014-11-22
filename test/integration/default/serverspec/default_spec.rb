bba19391fe0a7175a5441c6077067c75c97d2cf0
Phil Cohen <github@phlippers.net>
2014-11-22 08:35:55 -0800

    update `serverspec` setup, add a few expectations

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,21 @@
 require "serverspec"
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
 
-describe "newrelic-sysmond::default" do
-  it "installed `newrelic-sysmond`" do
-    expect(package "newrelic-sysmond").to be_installed
+set :backend, :exec
+
+describe "`newrelic-sysmond` installation" do
+  describe package("newrelic-sysmond") do
+    it { should be_installed }
+  end
+
+  describe service("newrelic-sysmond") do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe file("/etc/newrelic/nrsysmond.cfg") do
+    it { should be_a_file }
+    it { should be_owned_by "root" }
+    it { should be_grouped_into "newrelic" }
+    it { should be_mode 640 }
   end
 end
